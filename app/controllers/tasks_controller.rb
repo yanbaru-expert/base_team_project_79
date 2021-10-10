49e24b00a9dfbe08efdc58dfe8fb9fa82e1c7f3c
Tak-Ohta <sacredfield.tak@gmail.com>
2021-10-10 18:19:43 +0900

    タスクの削除機能を実装、編集完了時のメッセージ機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,11 @@ class TasksController < ApplicationController
   def update
     task = Task.find(params[:id])
     task.update(task_params)
-    redirect_to tasks_path
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
   end
 
   private
